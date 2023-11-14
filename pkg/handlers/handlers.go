d5c0eb2b87479ee078c23dcf00dd873ca7db43b0
Femi Oladele <femioladele@infotitans.com>
2023-11-13 19:18:55 -0800

    Implement Template Caching

--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -7,10 +7,10 @@ import (
 
 // Home is the handler for the home page
 func Home(w http.ResponseWriter, _ *http.Request) {
-	render.RenderTemplate(w, "home.page.tmpl")
+	render.ProcessTemplate(w, "home.page.tmpl")
 }
 
 // About is the handler for the about page
 func About(w http.ResponseWriter, _ *http.Request) {
-	render.RenderTemplate(w, "about.page.tmpl")
+	render.ProcessTemplate(w, "about.page.tmpl")
 }
--- pkg/render/render.go
+++ pkg/render/render.go
@@ -3,13 +3,48 @@ package render
 import (
 	"fmt"
 	"html/template"
+	"log"
 	"net/http"
 )
 
-func RenderTemplate(w http.ResponseWriter, tmpl string) {
-	parsedTemplate, _ := template.ParseFiles("./templates/" + tmpl)
-	err := parsedTemplate.Execute(w, nil)
-	if err != nil {
-		fmt.Println("error parsing template:", err)
+var templateMap = make(map[string]*template.Template)
+
+func ProcessTemplate(w http.ResponseWriter, t string) {
+	var tmpl *template.Template
+	var err error
+
+	// check to see if template already exist in our cache
+	_, inMap := templateMap[t]
+
+	if !inMap {
+		// need to create the template
+		err := createTemplateCache(t)
+		if err != nil {
+			log.Println(err)
+		}
 	}
+
+	tmpl = templateMap[t]
+	err = tmpl.Execute(w, nil)
+	if err != nil {
+		log.Println(err)
+	}
+}
+
+func createTemplateCache(t string) error {
+	templates := []string{
+		fmt.Sprintf("./templates/%s", t),
+		"./templates/base.layout.tmpl",
+	}
+
+	// parse the templates
+	tmpl, err := template.ParseFiles(templates...)
+	if err != nil {
+		log.Println(err)
+	}
+
+	// add template to cache(map)
+	templateMap[t] = tmpl
+
+	return nil
 }
